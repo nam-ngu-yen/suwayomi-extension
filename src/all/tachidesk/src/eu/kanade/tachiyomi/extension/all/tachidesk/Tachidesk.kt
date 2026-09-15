169f2af8f619822d03058f404d283b4d95f32a44
Nam Ngủyên <namnguyen.1695@gmail.com>
2026-09-15 15:49:38 +0700

    Use Chapter ID instead of Chapter Order as Chapter URL

--- src/all/tachidesk/src/eu/kanade/tachiyomi/extension/all/tachidesk/Tachidesk.kt
+++ src/all/tachidesk/src/eu/kanade/tachiyomi/extension/all/tachidesk/Tachidesk.kt
@@ -355,36 +355,22 @@ class Tachidesk : ConfigurableSource, UnmeteredSource, HttpSource() {
     override fun fetchPageList(chapter: SChapter): Observable<List<Page>> {
         return runCatching {
             val mangaId = chapter.url.substringBefore(' ', "").toInt()
-            val chapterSourceOrder = chapter.url.substringAfter(' ', "").toInt()
+            val chapterId = chapter.url.substringAfter(' ', "").toInt()
             apolloClient.value
-                .query(
-                    GetChapterIdQuery(mangaId, chapterSourceOrder),
+                .mutation(
+                    GetPagesMutation(chapterId),
                 )
                 .toFlow()
                 .map {
                     it.dataAssertNoErrors
-                        .chapters
-                        .nodes
-                        .single()
-                        .id
-                }
-                .flatMapLatest { chapterId ->
-                    apolloClient.value
-                        .mutation(
-                            GetPagesMutation(chapterId),
-                        )
-                        .toFlow()
-                        .map {
-                            it.dataAssertNoErrors
-                                .fetchChapterPages!!
-                                .pages
-                                .mapIndexed { index, url ->
-                                    Page(
-                                        index + 1,
-                                        "",
-                                        "$checkedBaseUrl$url",
-                                    )
-                                }
+                        .fetchChapterPages!!
+                        .pages
+                        .mapIndexed { index, url ->
+                            Page(
+                                index + 1,
+                                "",
+                                "$checkedBaseUrl$url",
+                            )
                         }
                 }
                 .asObservable()
@@ -933,7 +919,7 @@ class Tachidesk : ConfigurableSource, UnmeteredSource, HttpSource() {
     }
 
     private fun ChapterFragment.toSChapter() = SChapter.create().also {
-        it.url = "$mangaId $sourceOrder"
+        it.url = "$mangaId $id"
         it.name = name
         it.date_upload = uploadDate
         it.scanlator = scanlator
